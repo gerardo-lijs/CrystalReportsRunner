8f81c3cc218be0fff8931860f38b564e9abbd8de
Gerardo Lijs <gerardo@micropticsl.com>
2022-10-22 18:51:14 +0200

    add application icon

--- src/LijsDev.CrystalReportsRunner/ViewerForm.Designer.cs
+++ src/LijsDev.CrystalReportsRunner/ViewerForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewerForm));
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.SuspendLayout();
             // 
@@ -48,6 +49,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.crystalReportViewer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ViewerForm";
             this.Text = "Viewer Form";
             this.ResumeLayout(false);
