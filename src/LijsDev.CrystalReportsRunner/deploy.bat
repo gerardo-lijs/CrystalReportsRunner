a18f885268cde64c1789e05886f2abb912163791
gerardo-lijs <gerardo.lijs@gmail.com>
2022-10-25 16:30:14 +0200

    hide runtimes in NuGet packages

--- src/LijsDev.CrystalReportsRunner/deploy.bat
+++
@@ -1,5 +0,0 @@
-nuget pack LijsDev.CrystalReportsRunner.13.0.20.x64\LijsDev.CrystalReportsRunner.13.0.20.x64.nuspec
-nuget pack LijsDev.CrystalReportsRunner.13.0.20.x86\LijsDev.CrystalReportsRunner.13.0.20.x86.nuspec
-
-nuget pack LijsDev.CrystalReportsRunner.13.0.32.x64\LijsDev.CrystalReportsRunner.13.0.32.x64.nuspec
-nuget pack LijsDev.CrystalReportsRunner.13.0.32.x86\LijsDev.CrystalReportsRunner.13.0.32.x86.nuspec
